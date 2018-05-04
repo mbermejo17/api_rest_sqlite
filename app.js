06cd72f8190a562822652fdd2ec843efed822456
Manuel Bermejo Lopez <mbermejo17@gmail.com>
2018-05-04 18:29:20 +0200

    prueba

--- app.js
+++ app.js
@@ -16,11 +16,12 @@ User.Open();
 //User.CreateTable();
 //User.Close();
 //User.Open();
-User.Get('Pepe',(status,data)=>{
-  console.error(status);
-  console.log(data);
+User.Get('Pepe', (status, data) => {
+    console.error(status);
+    console.log(data);
 });
 User.Close();
+
 
 app.use(morgan("dev"));
 app.use('/uploads', express.static('uploads'));
@@ -28,16 +29,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
 });
 
 // Routes which should handle requests
@@ -46,18 +47,18 @@ app.use("/orders", orderRoutes);
 app.use("/user", userRoutes);
 
 app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
 });
 
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message
-    }
-  });
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
 });
 
 module.exports = app;
